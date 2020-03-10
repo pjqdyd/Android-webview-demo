d68dad55bc9b7cccc9c8dd6324aaacf090ee9238
pjqdyd <1909025079@qq.com>
2020-03-10 11:57:47 +0800

    文本输入框控件

--- app/src/main/java/com/pjqdyd/MainActivity.java
+++ app/src/main/java/com/pjqdyd/MainActivity.java
@@ -2,9 +2,11 @@ package com.pjqdyd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,20 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.btn);
-        //btn.setOnClickListener(new MyListener());
-        btn.setOnClickListener(view -> {
-            System.out.println("点击了一下");
-            Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
-        });
+        EditText editText = findViewById(R.id.editText);
+        EditText editPassword  = findViewById(R.id.editPassword);
+        //editText.setText("hello");
+        //editText.setTextColor(Color.BLUE);
 
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener((view) -> {
+            System.out.println(editText.getText().toString());
+            System.out.println(editPassword.getText().toString());
+            Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+        });
     }
 
-//    public class MyListener implements View.OnClickListener {
-//        @Override
-//        public void onClick(View view) {
-//            System.out.println("点击了一下");
-//            Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
-//        }
-//    }
 }
