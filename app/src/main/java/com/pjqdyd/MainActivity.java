2624af1560b7e34ea00fc88ce98f437e837a0fad
pjqdyd <1909025079@qq.com>
2020-03-22 21:00:51 +0800

    ImageView图片组件

--- app/src/main/java/com/pjqdyd/MainActivity.java
+++ app/src/main/java/com/pjqdyd/MainActivity.java
@@ -2,63 +2,33 @@ package com.pjqdyd;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.Toast;
-import java.util.HashMap;
-import java.util.Map;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity  implements OnCheckedChangeListener {
+import java.util.Arrays;
+import java.util.List;
 
-    private CheckBox cbEat;
-    private CheckBox cbSleep;
-    private CheckBox cbCoding;
-    private Map<String, String> likeMap = new HashMap();
+public class MainActivity extends AppCompatActivity {
+    private static final List<Integer> imageList = Arrays.asList(R.drawable.img1, R.drawable.img2, R.drawable.img3);
+    private int imageIndex = 0; //显示的图片下标
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 获取控件
-        cbEat =  findViewById(R.id.cbEat);
-        cbSleep = findViewById(R.id.cbSleep);
-        cbCoding = findViewById(R.id.cbCoding);
-        // 绑定事件
-        cbEat.setOnCheckedChangeListener(this);
-        cbSleep.setOnCheckedChangeListener(this);
-        cbCoding.setOnCheckedChangeListener(this);
-    }
 
-    @Override
-    public void onCheckedChanged(CompoundButton checkBox, boolean checked) {
-        switch (checkBox.getId()) {
-            case R.id.cbEat:
-                likeMap.put("eat", checked ? "吃" : "");
-                break;
-            case R.id.cbSleep:
-                likeMap.put("sleep", checked ? "睡觉" : "");
-                break;
-            case R.id.cbCoding:
-                likeMap.put("coding", checked ? "敲代码" : "");
-                break;
-            default:
-                break;
-        }
-    }
+        ImageView imageView = findViewById(R.id.image_view);
 
-    public void submit(View v) {//当按钮点击后
-        // 循环读取爱好
-        StringBuilder likeStr = new StringBuilder();
-        if (likeMap.size() == 0) {
-            String no = "这个人完全没有爱好!";
-            Toast.makeText(MainActivity.this, no, Toast.LENGTH_SHORT).show();
-        } else {
-            //likeMap.entrySet().stream().filter((e) -> e.getValue() != "").forEach(e -> likeStr.append(e.getValue()));
-            likeMap.forEach((key, value) -> likeStr.append(" ").append(value));
-            Toast.makeText(MainActivity.this, "你的爱好: " + likeStr, Toast.LENGTH_SHORT).show();
-        }
-    }
+        Button btnPrevious = findViewById(R.id.btn_previous); //上一张
+        btnPrevious.setOnClickListener(view -> {
+            imageIndex = imageIndex-1 < 0 ? 2 : imageIndex-1;
+            imageView.setImageResource(imageList.get(imageIndex));
 
+        });
+        Button btnNext = findViewById(R.id.btn_next); //下一张
+        btnNext.setOnClickListener(view -> {
+            imageIndex = imageIndex+1 > 2 ? 0 : imageIndex+1;
+            imageView.setImageResource(imageList.get(imageIndex));
+        });
+    }
 }
